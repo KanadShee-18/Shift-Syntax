cb6fcef5612e8999531fd1b0d3bba8c92e5ad5ff
Kanad <codewizardkanad@gmail.com>
2025-07-13 22:31:45 +0530

    Implement lazy loading for some compoenents

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,63 @@
 import HomeSection from "./components/HeroSection";
 import { Routes, Route } from "react-router-dom";
-import SignUp from "./components/SignUp";
-import SignIn from "./components/SignIn";
+// import SignUp from "./components/SignUp";
+// import SignIn from "./components/SignIn";
 import { Toaster } from "sonner";
 import PrivateRoute from "./components/PrivateRoute";
 import OpenRoute from "./components/OpenRoute";
-import CodeShift from "./components/codeConvert/CodeShift";
-import About from "./components/About";
+// import CodeShift from "./components/codeConvert/CodeShift";
+import { lazy } from "react";
 import Navbar from "./components/Navbar";
+import { Suspense } from "react";
+import { Loader2Icon } from "lucide-react";
+
+const About = lazy(() => import("./components/About"));
+const SignUp = lazy(() => import("./components/SignUp"));
+const SignIn = lazy(() => import("./components/SignIn"));
+const CodeShift = lazy(() => import("./components/codeConvert/CodeShift"));
 
 const App = () => {
   return (
     <div className="selection:bg-primary/30">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomeSection />} />
-        <Route path="/about" element={<About />} />
-        <Route
-          path="/sign-in"
-          element={
-            <OpenRoute>
-              <SignIn />
-            </OpenRoute>
-          }
-        />
-        <Route
-          path="/sign-up"
-          element={
-            <OpenRoute>
-              <SignUp />
-            </OpenRoute>
-          }
-        />
-        <Route path="/contact" element={<div>contact</div>} />
-        <Route
-          path="/code-shift"
-          element={
-            <PrivateRoute>
-              <CodeShift />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="grid h-[60vh] place-items-center">
+            <Loader2Icon className="animate-spin" />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<HomeSection />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/sign-in"
+            element={
+              <OpenRoute>
+                <SignIn />
+              </OpenRoute>
+            }
+          />
+          <Route
+            path="/sign-up"
+            element={
+              <OpenRoute>
+                <SignUp />
+              </OpenRoute>
+            }
+          />
+          <Route path="/contact" element={<div>contact</div>} />
+          <Route
+            path="/code-shift"
+            element={
+              <PrivateRoute>
+                <CodeShift />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
+
       <Toaster richColors position="bottom-left" className="font-sans" />
     </div>
   );
