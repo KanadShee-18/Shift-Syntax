fc138d5d92b185cd6855e29f6ff212971eae84c8
Kanad <codewizardkanad@gmail.com>
2025-07-13 22:57:32 +0530

    Removed unused code from backend controller

--- server/src/routes/chat.route.js
+++ server/src/routes/chat.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { generateChat, convertCode } from "../controllers/chat.controller.js";
+import { convertCode } from "../controllers/chat.controller.js";
 import { isUserAuthenticated } from "../middlewares/auth.middleware.js";
 
 router.post("/query", isUserAuthenticated, convertCode);
